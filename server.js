04c1aeff5e8354dbf6384bfd40f1ffd7da2de887
israel <lisrael1989@gmail.com>
2024-03-18 20:54:36 +0200

    connect the back to public

--- server.js
+++ server.js
@@ -1,25 +1,36 @@
 import express from "express";
 import { bugService } from "./services/bugs.service.js";
-// import { bugService } from "./services/bugs.service.js";
-// import { loggerService } from "./services/logger.service.js";
+import { loggerService } from "./services/logger.service.js";
 // import cookieParser from "cookie-parser";
 
 const app = express();
 
-// app.use(express.static("public"));
+app.use(express.static("public"));
 // app.use(cookieParser())
 
 /*get bugs */
 app.get("/api/bug", (req, res) => {
-  bugService.query().then((bugs) => {
-    res.send(bugs);
-  });
+  bugService
+    .query()
+    .then((bugs) => {
+      res.send(bugs);
+    })
+    .catch((err) => {
+      loggerService.error("Cannot save bug", err);
+      res.status(400).send("Cannot get bug");
+    });
 });
 
 //get bug
 app.get("/api/bug/:id", (req, res) => {
   const bugId = req.params.id;
-  bugService.getById(bugId).then((bug) => res.send(bug));
+  bugService
+    .getById(bugId)
+    .then((bug) => res.send(bug))
+    .catch((err) => {
+      loggerService.error("Cannot save bug", err);
+      res.status(400).send("Cannot save bug");
+    });
 });
 
 //save bug
@@ -31,13 +42,25 @@ app.get("/api/bug/save", (req, res) => {
     _id: req.query._id,
     createdAt: req.query.createdAt,
   };
-  bugService.save(bugToSave).then((bug) => res.send(bug));
+  bugService
+    .save(bugToSave)
+    .then((bug) => res.send(bug))
+    .catch((err) => {
+      loggerService.error("Cannot save bug", err);
+      res.status(400).send("Cannot get bug");
+    });
 });
 
 //remove bug
 app.get("/api/bug/:id/remove", (req, res) => {
   const bugId = req.params.id;
-  bugService.remove(bugId).then((bugs) => res.send(bugs));
+  bugService
+    .remove(bugId)
+    .then((bugs) => res.send(bugs))
+    .catch((err) => {
+      loggerService.error("Cannot save bug", err);
+      res.status(400).send("Cannot get bug");
+    });
 });
 
 const port = 3000;
