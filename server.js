5ba079890aa2b42d72f55d70c725dba8b7a02b93
israel <lisrael1989@gmail.com>
2024-03-18 20:22:29 +0200

    add remove + save to server

--- server.js
+++ server.js
@@ -9,10 +9,35 @@ const app = express();
 // app.use(express.static("public"));
 // app.use(cookieParser())
 
+/*get bugs */
 app.get("/api/bug", (req, res) => {
   bugService.query().then((bugs) => {
     res.send(bugs);
   });
+});
+
+//get bug
+app.get("/api/bug/:id", (req, res) => {
+  const bugId = req.params.id;
+  bugService.getById(bugId).then((bug) => res.send(bug));
+});
+
+//save bug
+app.get("/api/bug/save", (req, res) => {
+  const bugToSave = {
+    title: req.query.title,
+    severity: +req.query.severity,
+    description: req.query.description,
+    _id: req.query._id,
+    createdAt: req.query.createdAt,
+  };
+  bugService.save(bugToSave).then((bug) => res.send(bug));
+});
+
+//remove bug
+app.get("/api/bug/:id/remove", (req, res) => {
+  const bugId = req.params.id;
+  bugService.remove(bugId).then((bugs) => res.send(bugs));
 });
 
 const port = 3000;
--- services/bugs.service.js
+++ services/bugs.service.js
@@ -4,9 +4,9 @@ import { utilService } from "./utils.service.js";
 
 export const bugService = {
   query,
-  // getById,
-  // remove,
-  // save,
+  getById,
+  remove,
+  save,
 };
 
 const bugs = utilService.readJsonFile("data/bug.json");
@@ -15,40 +15,39 @@ function query() {
   return Promise.resolve(bugs);
 }
 
-// function getById(id) {
-//     const car = cars.find(car => car._id === id)
-//     if (!car) return Promise.reject('Car does not exist!')
-//     return Promise.resolve(car)
-// }
+function getById(id) {
+  const bug = bugs.find((bug) => bug._id === id);
+  if (!bug) return Promise.reject("bug does not exist!");
+  return Promise.resolve(bug);
+}
 
-// function remove(id) {
-//     const carIdx = cars.findIndex(car => car._id === id)
-//     cars.splice(carIdx, 1)
-//     return _saveCarsToFile()
+function remove(id) {
+  const bugIdx = bugs.findIndex((bug) => bug._id === id);
+  bugs.splice(bugIdx, 1);
+  return _saveBugsToFile();
+}
 
-// }
+function save(bug) {
+  if (bug._id) {
+    const bugIdx = bugs.findIndex((_bug) => _bug._id === bug._id);
+    bugs[bugIdx] = bug;
+  } else {
+    bug._id = utilService.makeId();
+    bug.description = utilService.makeLorem();
+    bugs.unshift(bug);
+  }
+  return _saveBugsToFile().then(() => bug);
+}
 
-// function save(car) {
-//     if (car._id) {
-//         const carIdx = cars.findIndex(_car => _car._id === car._id)
-//         cars[carIdx] = car
-//     } else {
-//         car._id = utilService.makeId()
-//         car.desc = utilService.makeLorem()
-//         cars.unshift(car)
-//     }
-//     return _saveCarsToFile().then(() => car)
-// }
-
-// function _saveCarsToFile() {
-//     return new Promise((resolve, reject) => {
-//         const data = JSON.stringify(cars, null, 4)
-//         fs.writeFile('data/car.json', data, (err) => {
-//             if (err) {
-//                 console.log(err)
-//                 return reject(err)
-//             }
-//             resolve()
-//         })
-//     })
-// }
+function _saveBugsToFile() {
+  return new Promise((resolve, reject) => {
+    const data = JSON.stringify(bugs, null, 4);
+    fs.writeFile("data/bug.json", data, (err) => {
+      if (err) {
+        console.log(err);
+        return reject(err);
+      }
+      resolve();
+    });
+  });
+}
